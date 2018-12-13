833c14ab257789074853044d82c6dad668d6cb23
Murillo Batalha <iventis.murillo@hubfintech.com.br>
2018-12-13 15:38:03 -0200

    vai

--- src/Startup.cs
+++ src/Startup.cs
@@ -19,7 +19,7 @@ namespace PocWeb
         {
             Configuration = configuration;
         }
-
+ 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
